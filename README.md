9b77380e8bfc64df0ccaf5164de1923cbe2ba31c
Ammar Hassan <ammar@ammarhassan.co.uk>
2025-11-18 17:26:51 +0000

    basic calculator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# js-calculator
+<!-- 
+Give two numbers and couple of button which is basic calculation, the result is show
+ -->
---
+++ app.js
@@ -0,0 +1,25 @@
+const firstInput = document.getElementById('firstTextField');
+const secondInput = document.getElementById('secondTextField');
+const sum = document.getElementById('sum');
+const add = document.getElementById('add');
+const subtract = document.getElementById('subtract');
+const divide = document.getElementById('divide');
+const multiply = document.getElementById('multiply');
+
+firstInput.value = 8;
+secondInput.value = 6;
+
+
+add.addEventListener("click", ()=>{
+    sum.innerText = Number(firstInput.value) +  Number(secondInput.value);
+})
+subtract.addEventListener("click", ()=>{
+    sum.innerText = Number(firstInput.value) -  Number(secondInput.value);
+})
+divide.addEventListener("click", ()=>{
+    sum.innerText = Number(firstInput.value) / Number(secondInput.value);
+})
+multiply.addEventListener("click", ()=>{
+    const decimal = Number(firstInput.value) *  Number(secondInput.value);
+    sum.innerText = decimal.toFixed(2)
+})
